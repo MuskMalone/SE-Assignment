4321331a7a783443fd4f6e87b30482e7464fd391
Filurn <41350682+Filurn@users.noreply.github.com>
2020-02-08 03:46:47 +0800

    Order State Machine

---
+++ SE Assignment/SE Assignment/DispatchedState.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Assignment
+{
+    class DispatchedState : OrderState
+    {
+        /*private Order myOrder;
+
+        public DispatchedState(Order order)
+        {
+            myOrder = order;
+        }*/
+
+        public void prepareOrder()
+        {
+            Console.WriteLine("Order preparation has already started!");
+        }
+
+        public void completeOrder()
+        {
+            Console.WriteLine("Order already complete!");
+        }
+
+        public void dispatchOrder()
+        {
+            Console.WriteLine("Order has already been dispatched");
+        }
+
+        public void confirmOrder()
+        {
+            Console.WriteLine("Order has been received");
+            // Code
+        }
+
+        public void archiveOrder()
+        {
+            Console.WriteLine("Order has been cancelled");
+            // Code
+        }
+    }
+}
---
+++ SE Assignment/SE Assignment/OrderState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE_Assignment
+{
+    interface OrderState
+    {
+        void prepareOrder();
+
+        void completeOrder();
+
+        void dispatchOrder();
+
+        void confirmOrder();
+
+        void archiveOrder();
+    }
+}
