f205be167fa283886a8abeb432e7abe3912785e3
danielravindran <s10181158@connect.np.edu.sg>
2020-02-08 09:57:38 +0800

    added partial contents for order

---
+++ SE Assignment/SE Assignment/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace SE_Assignment
+{
+    public class Order
+    {
+
+
+        private string orderStatus;
+        public String OrderStatus { get; set; }
+
+
+        public void registerCustomer()
+        {
+            Console.WriteLine("");
+            // Code
+        }
+
+        public void removeCustomer()
+        {
+            Console.WriteLine("");
+            // Code
+        }
+
+        public void notifyCustomer()
+        {
+            Console.WriteLine("");
+            // Code
+        }
+
+
+        public Order()
+        {
+        }
+
+    }
+}
