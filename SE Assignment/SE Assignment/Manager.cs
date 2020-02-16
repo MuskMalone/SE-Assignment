00208ef69eba3baee477aae360186f78f812f47e
Filurn <41350682+Filurn@users.noreply.github.com>
2020-02-16 18:29:31 +0800

    Update

--- SE Assignment/SE Assignment/Manager.cs
+++ SE Assignment/SE Assignment/Manager.cs
@@ -58,5 +58,40 @@ namespace SE_Assignment
             Console.WriteLine("Created Menu Successfully!");
             return mc;
         }
+
+        public void displayAllFood(FoodIterator menu, int type)
+        {
+            // 1 - Alacarte 2 - SetMenus 0 - All
+            if (type == 1)
+            {
+                if (menu.GetCurrent().IsSetMenu == false)
+                    Console.WriteLine(menu.GetCurrent().NewToString(true));
+                while (menu.HasNextFood())
+                {
+                    if (menu.NextFood().IsSetMenu == false)
+                        Console.WriteLine(menu.GetCurrent().NewToString(true));
+                }
+            }
+            else if (type ==2)
+            {
+                if (menu.GetCurrent().IsSetMenu == false)
+                    Console.WriteLine(menu.GetCurrent().NewToString(true));
+                while (menu.HasNextFood())
+                {
+                    if (menu.NextFood().IsSetMenu == false)
+                        Console.WriteLine(menu.GetCurrent().NewToString(true));
+                }
+            }
+            else if (type == 0)
+            {
+                Console.WriteLine(menu.GetCurrent().NewToString(true));
+                while (menu.HasNextFood())
+                {
+                    Console.WriteLine(menu.NextFood().NewToString(true));
+
+                }
+            }
+
+        }
     }
 }
